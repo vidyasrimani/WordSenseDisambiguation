c288ef925d5d1544c6ce7b456d89c9a69da7a1a4
Vidya Mani <sm.vidya@gmail.com>
2017-11-15 20:48:19 -0600

    Problem Statement modified

---
+++ simplifiedLesk.py
@@ -0,0 +1,12 @@
+"""
+Vidya Sri Mani
+vxm163230
+
+
+Using WordNet as the dictionary, implement the SIMPLIFIED LESK algorithm to
+disambiguate the word bank in the sentence. Your output should show the word
+overlap for each sense of the word bank in WordNet and the final chosen sense.
+The bank can guarantee deposits will eventually cover future tuition costs
+because it invests in adjustable-rate mortgage securities.
+
+"""
